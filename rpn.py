4cb5ec4e7495ffd9cdd31974d9cfcadac35649c9
derektan <derektan@umich.edu>
2019-03-13 22:50:12 -0400

    created travis file

--- rpn.py
+++ rpn.py
@@ -1,10 +1,55 @@
-#!/usr/bin/env python3
-#don't need semicolon!
-#very useful for prototyping using simple code
+#!/usr/bin/env pythn3
+#don't need semicon!
+#very useful for prototyping
 
-def calculate(arg):      #':' with indentation means block
-    pass                 # means do nothing. auto exit block
-                         # when indentation restores
+class Stack:
+     def __init__(self):
+         self.items = []
+
+     def isEmpty(self):
+         return self.items == []
+
+     def push(self, item):
+         self.items.append(item)
+
+     def pop(self):
+         return self.items.pop()
+
+     def peek(self):
+         return self.items[len(self.items)-1]
+
+     def size(self):
+         return len(self.items)
+
+def calculate(arg1, arg2, operator):      #':' with indentation means block
+    my_stack = Stack()
+#    print ("stack created")
+#    for token in arg.split():
+#        try:
+#            token = int(token)
+#            my_stack.push(token)
+#        except ValueError:
+    
+    my_stack.push(arg1)
+    my_stack.push(arg2)
+
+    first_num = my_stack.pop()
+    sec_num = my_stack.pop()
+
+    if operator == '+': 
+        my_stack.push(first_num + sec_num)
+#        print ("plus sign called")
+        return first_num + sec_num
+    elif operator == '-':
+        my_stack.push(first_num - sec_num)
+#        print ("minus sign called")
+        return first_num - sec_num
+
+#          function = operators[token]
+#            arg2 = my_stack.pop()
+#            arg1 = my_stack.pop()
+#            result = function(arg1, arg2)
+#            my_stack.append(result)
 
 def main():
     while True:
@@ -13,5 +58,3 @@ def main():
 
 if __name__ == '__main__':       #no need for if (...)
     main()                      # means that you run 'main' whenever filename is called
-
-
--- test_rpn.py
+++ test_rpn.py
@@ -5,24 +5,24 @@ import rpn                      # something like #include in c++
 class TestBasics(unittest.TestCase):        #class definition using unit test framework
 
     def test_plus(self):                     # test 1
-        result = rpn.calculate('1 1 +')     # no need to define 'result'?
+        result = rpn.calculate(1, 1, '+')     # no need to define 'result'?
         self.assertEqual(2,result)          # check if 2 == result
 
     def test_minus(self):                     # test 1
-        result = rpn.calculate('1 1 -')     # no need to define 'result'?
+        result = rpn.calculate(1, 1, '-')     # no need to define 'result'?
         self.assertEqual(0,result)          # check if 0 == result
 
     def test_minus(self):                     # test 1
-        result = rpn.calculate('1 1 -')     # no need to define 'result'?
-        self.assertEqual(0,result)          # check if 0 == result
+        result = rpn.calculate(3, 1, '-')     # no need to define 'result'?
+        self.assertEqual(-2,result)          # check if 0 == result
 
     def test_prev_calc(self):                # test 1
-        result = rpn.calculate('2 3 +')     # no need to define 'result'?
+        result = rpn.calculate(2, 3, '+')     # no need to define 'result'?
         self.assertEqual(5,result)          # check if 0 == result
 
-    def test_minus(self):                     # test 1
-        result = rpn.calculate('ans 7 +')     # no need to define 'result'?
-        self.assertEqual(12,result)          # check if 0 == result
+ #   def test_minus(self):                     # test 1
+ #       result = rpn.calculate("ans 7 +")     # no need to define 'result'?
+ #       self.assertEqual(12,result)          # check if 0 == result
 
 
 
